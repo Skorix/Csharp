00efb5126d858ae681a5b6f239c161d9c03c62d1
Skorix <54755559+Skorix@users.noreply.github.com>
2021-02-19 03:20:19 +0300

    Add files via upload

---
+++ DZ_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            double x, y;
+            
+            st:
+            x = Convert.ToDouble(Console.ReadLine());
+            if ((x % 1) != 0)
+            {
+                Console.WriteLine("вы ввели не целое число");
+                goto st;
+            }
+
+            st1:
+            y = Convert.ToDouble(Console.ReadLine());
+            if ((x % 1) != 0)
+            {
+                Console.WriteLine("вы ввели не целое число");
+                goto st1;
+            }
+            
+            Console.WriteLine("Ответ: " + Convert.ToString(Convert.ToInt32(x) + Convert.ToInt32(y)));
+            Console.ReadKey();
+
+        }
+    }
+}

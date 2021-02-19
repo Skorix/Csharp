d97a49cfa03001d2eb8b2b41ddc6f13d5252e9f3
Skorix <54755559+Skorix@users.noreply.github.com>
2021-02-19 03:14:35 +0300

    Add files via upload

---
+++ 37IS_Lab2/Form2.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace _37IS_Lab2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.memberList = new System.Windows.Forms.CheckedListBox();
+            this.peopleList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.memberList);
+            this.groupBox1.Location = new System.Drawing.Point(269, 117);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 175);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "список участников";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(506, 117);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(506, 355);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // memberList
+            // 
+            this.memberList.FormattingEnabled = true;
+            this.memberList.Location = new System.Drawing.Point(7, 22);
+            this.memberList.Name = "memberList";
+            this.memberList.Size = new System.Drawing.Size(187, 123);
+            this.memberList.TabIndex = 0;
+            // 
+            // peopleList
+            // 
+            this.peopleList.FormattingEnabled = true;
+            this.peopleList.Items.AddRange(new object[] {
+            "Иванов",
+            "Петров",
+            "Сидоров"});
+            this.peopleList.Location = new System.Drawing.Point(269, 354);
+            this.peopleList.Name = "peopleList";
+            this.peopleList.Size = new System.Drawing.Size(200, 24);
+            this.peopleList.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(266, 295);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 56);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Выберите фамилию участника или введите новую";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 39);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.peopleList);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckedListBox memberList;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.ComboBox peopleList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ 37IS_Lab2/Form4.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _37IS_Lab2
+{
+    public partial class Form4 : Form
+    {
+        private int progress=0;
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            switch (Convert.ToString(listBox1.SelectedItem))
+            {
+                default:
+                    timer1.Interval = 1000;
+                    break;
+                case "Минуты":
+                    timer1.Interval = 60000;
+                    break;
+                case "Часы":
+                    timer1.Interval = 3600000;
+                    break;
+
+            }
+
+            timer1.Interval = (timer1.Interval * Convert.ToInt32(numericUpDown1.Value))/100;
+            MessageBox.Show(Convert.ToString(timer1.Interval = (timer1.Interval * Convert.ToInt32(numericUpDown1.Value))));
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value++;
+            if (progressBar1.Value == 100)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                MessageBox.Show("Win");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            timer1.Enabled = false;
+        }
+        public int qwe = 0;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            qwe++;
+            label1.Text = Convert.ToString(qwe);
+        }
+    }
+}
